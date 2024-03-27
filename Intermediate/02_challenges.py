21869181bc5c0ecb763d881755b4a43c7c30bd5f
leydi Franco <leydilufranco@gmail.com>
2024-03-26 23:29:19 -0600

    Challenges

--- Intermediate/02_challenges.py
+++ Intermediate/02_challenges.py
@@ -36,4 +36,60 @@ def is_anagram (word_one, word_two):
     if word_one.lower() == word_two.lower():
         return False
     return sorted (word_one.lower()) == sorted(word_two.lower())
-print (is_anagram("Amor", "Roma"))
+print (is_anagram("Omare", "Roma"))
+
+"""
+Escribe un programa que imprima los 50 primeros numeros de la sucesion de Fibonacci empezando en 0 
+- La serie Fibonacci se compone por una sucesion de numeros en la que el siguiente siempre es la suma de los anteriores
+"""
+
+def fibonacci ():
+    prev=0 
+    next = 1
+    
+    for index in range (0, 50):
+        print (prev)
+        fib = prev + next 
+        prev = next 
+        next = fib
+
+fibonacci()
+
+"""
+Es un numero primo?
+Escribe un programa que se encargue de comprobar si un numero es primo o no
+hecho esto imprime los numeros primos entre 1 y 100
+"""
+
+def primos ():
+
+    for number in range (1, 101):
+
+        if number >= 2:
+            is_divisible = False 
+    
+            for index in range (2, number):
+                if number % index == 0: 
+                    is_divisible = True
+                    break
+            if not is_divisible: 
+                print(number)
+
+primos()
+
+"""
+Crea un programa que invierta el orden de una cadena de texto sin usar 
+funciones propias del lenguaje que lo haga de forma automatica 
+- si le pasamos "hola mundo" nos retorna "odnum aloh"
+"""
+
+
+def reverse (text): 
+    text_len = len(text)
+    reversed_text = ""
+
+    for index in range (0, len(text)):
+        reversed_text += text[text_len -index -1]
+    return reversed_text
+
+print (reverse("Hola Mundo"))
