0592f3e29b3f990ce129a8d8bc2a9cdb66ddbbcf
leydi Franco <leydilufranco@gmail.com>
2024-03-28 16:24:21 -0600

    Lambda Class

---
+++ Intermediate/04_higher_order_funtions.py
@@ -0,0 +1,12 @@
+## Higher order Functions 
+
+def sum_one (value):
+    return value + 1
+
+def mult_one (value):
+    return value * 5
+
+def sum_two_and_add_one (first_value, second_value, mul_value):
+    return mul_value (first_value + second_value)
+
+print (sum_two_and_add_one(2, 4, mult_one))
