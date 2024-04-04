02f6a7e2dc65f66b28d4674e4a2ce51f17357608
leydi Franco <leydilufranco@gmail.com>
2024-04-04 09:51:53 -0600

    instalar librerias

--- Intermediate/07_package_manager.py
+++ Intermediate/07_package_manager.py
@@ -1,4 +1,14 @@
 ## phthon package
 
-## https://pypi.org
+##https://pypi.org
 
+import numpy
+import requests ## se debe instalar con pip install numpy
+
+numpy_array = numpy.array ([33, 21, 62, 52, 23, 23, 12])
+print (numpy_array * 2)
+
+response = requests.get ("https://pokeapi.co/api/v2/pokemon?limit=151")
+print (response)
+print (response.status_code)
+print (response.json)
