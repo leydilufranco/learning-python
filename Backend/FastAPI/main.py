0e0a60cf668427904465877dd3e09a123fb9a638
leydi Franco <leydilufranco@gmail.com>
2024-04-08 21:11:33 -0600

    Servicio

---
+++ Backend/FastAPI/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root ():
+    return '¡Hola Leydi Linda!'
+
+
+@app.get("/url")
+async def root():
+    return {"url":"https://leydifranco.com/python"}
+
+
+## Iniciar el server: uvicorn main:app --reload
+## Detener el server CTR+C
+## Documentacion con Swagger http://127.0.0.1:8000/docs
+## Documentacion con Redocly http://127.0.0.1:8000/redoc
---
+++ Backend/type_hints.py
@@ -0,0 +1,9 @@
+## Tyoe Hints 
+
+my_string_variable = "My String Variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5 
+print (my_string_variable)
+print(type(my_string_variable))
